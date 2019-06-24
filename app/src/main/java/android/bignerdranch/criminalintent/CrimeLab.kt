60c1687bedb8d897612ee53cb4ef4bf84f738e1b
jason.satterly <jason.satterly@cdinteractive.com>
2019-06-24 13:35:10 -0400

    CH14 - SQLite - 14.11 pushing updates

--- app/src/main/java/android/bignerdranch/criminalintent/CrimeLab.kt
+++ app/src/main/java/android/bignerdranch/criminalintent/CrimeLab.kt
@@ -1,28 +1,55 @@
 package android.bignerdranch.criminalintent
 
+import android.bignerdranch.criminalintent.database.CrimeBaseHelper
+import android.bignerdranch.criminalintent.database.CrimeTable
+import android.content.ContentValues
 import android.content.Context
 import java.util.*
 
 class CrimeLab private constructor(context: Context) {
 
-    private val crimes = mutableListOf<Crime>()
+    private val database = CrimeBaseHelper(context).writableDatabase
 
     fun addCrime(crime: Crime) {
-        crimes.add(crime)
+        val values = getContentValues(crime)
+        database.insert(CrimeTable.NAME, null, values)
+    }
+
+    fun updateCrime(crime: Crime) {
+        val uuidString = crime.id.toString()
+        val values = getContentValues(crime)
+
+        database.update(CrimeTable.NAME,
+            values,
+            "${CrimeTable.Cols.UUID} = ?",
+            arrayOf(uuidString))
     }
 
     fun deleteCrime(crime: Crime){
-        val index = crimes.indexOf(crime)
-        if (index >= 0) crimes.removeAt(index)
+        val uuidString = crime.id.toString()
+        database.delete(CrimeTable.NAME, "${CrimeTable.Cols.UUID} = ?", arrayOf(uuidString))
     }
 
     fun getCrimes(): List<Crime> {
-        return crimes
+        return listOf()
     }
 
     fun getCrime(id: UUID): Crime? {
-        return crimes.find {
-            it.id == id
+        return null
+    }
+
+    private fun getContentValues(crime: Crime): ContentValues {
+        val solved = if (crime.isSolved) {
+            1
+        } else {
+            0
+        }
+
+        return ContentValues().apply {
+            put(CrimeTable.Cols.UUID, crime.id.toString())
+            put(CrimeTable.Cols.TITLE, crime.title)
+            put(CrimeTable.Cols.DATE, crime.date.time)
+            put(CrimeTable.Cols.SOLVED, solved)
         }
     }
 
