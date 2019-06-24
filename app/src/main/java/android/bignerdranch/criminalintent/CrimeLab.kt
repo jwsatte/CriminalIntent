d1b7d660233d13ee65b076e3e950133ea141dd72
jason.satterly <jason.satterly@cdinteractive.com>
2019-06-24 14:05:55 -0400

    CH14 - SQLite - 14.15 using the SQLite query

--- app/src/main/java/android/bignerdranch/criminalintent/CrimeLab.kt
+++ app/src/main/java/android/bignerdranch/criminalintent/CrimeLab.kt
@@ -1,9 +1,11 @@
 package android.bignerdranch.criminalintent
 
 import android.bignerdranch.criminalintent.database.CrimeBaseHelper
+import android.bignerdranch.criminalintent.database.CrimeCursorWrapper
 import android.bignerdranch.criminalintent.database.CrimeTable
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import java.util.*
 
 class CrimeLab private constructor(context: Context) {
@@ -25,13 +27,37 @@ class CrimeLab private constructor(context: Context) {
             arrayOf(uuidString))
     }
 
+    private fun queryCrimes(whereClause: String?, whereArgs: Array<String>?): CrimeCursorWrapper {
+        val cursor = database.query(
+            CrimeTable.NAME,
+            null, // null selects all columns
+            whereClause,
+            whereArgs,
+            null,
+            null,
+            null)
+
+        return CrimeCursorWrapper(cursor)
+    }
+
     fun deleteCrime(crime: Crime){
         val uuidString = crime.id.toString()
         database.delete(CrimeTable.NAME, "${CrimeTable.Cols.UUID} = ?", arrayOf(uuidString))
     }
 
     fun getCrimes(): List<Crime> {
-        return listOf()
+
+        val crimes = mutableListOf<Crime>()
+        val cursor = queryCrimes(null, null)
+        cursor.use {
+            it.moveToFirst()
+            while (!it.isAfterLast) {
+                crimes.add(it.getCrime())
+                it.moveToNext()
+            }
+        }
+
+        return crimes
     }
 
     fun getCrime(id: UUID): Crime? {
